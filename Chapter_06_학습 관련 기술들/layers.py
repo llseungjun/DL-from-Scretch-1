4de7d7a13a13075288685121f590237c78d89984
llseungjun <sjl27552755@gmail.com>
2024-07-22 16:33:49 +0900

    Chapter 6 내용 정리 나머지

---
+++ Chapter_06_학습 관련 기술들/layers.py
@@ -0,0 +1,189 @@
+import numpy as np
+from utils import softmax, cross_entropy_error, numerical_gradient
+from collections import OrderedDict # 순서가 보장된 딕셔너리
+
+class MulLayer:
+    """곱셈 layer에서 forward, backward 연산
+    """
+    def __init__(self):
+        self.x = None
+        self.y = None
+    
+    def forward(self,x,y):
+        self.x = x
+        self.y = y
+        out  = x * y
+        
+        return out
+    
+    def backward(self, dout):
+        dx = dout*self.y
+        dy = dout*self.x
+
+        return dx, dy
+    
+class AddLayer:
+    """덧셈 layer에서 forward, backward 연산
+    """
+    def __init__(self) -> None:
+        pass
+    
+    def forward(self,x,y):
+        out = x + y
+        return out
+    
+    def backward(self,dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+class Relu:
+    def __init__(self) -> None:
+        self.mask = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+    
+class Sigmoid:
+    def __init__(self) -> None:
+        self.out = None
+
+    def forward(self, x):
+        self.out = 1 / (1 + np.exp(-x))
+
+        return self.out
+    
+    def backward(self, dout):
+        dx = dout * self.out * (1.0 - self.out)
+
+        return dx
+
+class Affine:
+    def __init__(self, W, b) -> None:
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x,self.W) + self.b
+
+        return out
+    
+    def backward(self, dout):
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis = 0)
+        dx = np.dot(dout, self.W.T)
+
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self) -> None:
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+    
+    def backward(self, dout = 1):
+        # batch_size = self.t.shape[0]
+        # # batch_size로 나누는 이유는 ? -> 일종의 정규화 과정이라고 함.. 
+        # # 책에는 정확하게 설명이 안 나와 있다..
+        # dx = (self.y - self.t) / batch_size
+        # reference: https://github.com/llseungjun/deep-learning-from-scratch/blob/master/common/layers.py
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size: # 정답 레이블이 원-핫 인코딩 형태일 때
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+        
+        return dx
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01) -> None:
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        # layer 생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'],self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'],self.params['b2'])
+        
+        self.lastlayer = SoftmaxWithLoss()
+
+    def predict(self,x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        
+        return x
+    
+    # x: 입력 데이터, t: 정답 레이블
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastlayer.forward(y, t)
+    
+    def accuaracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis = 1)
+        
+        if t.ndim != 1: # t가 원핫 인코딩일 경우
+            t = np.argmax(t, axis = 1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x,t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+    
+    def gradient(self, x, t):
+        # 순전파
+        self.loss(x, t)
+
+        # 역전파
+        dout = 1
+        dout = self.lastlayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+
+        return grads
---
+++ Chapter_06_학습 관련 기술들/utils.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def smooth_curve(x):
+    # reference: https://github.com/llseungjun/deep-learning-from-scratch/blob/master/common/util.py
+    """손실 함수의 그래프를 매끄럽게 하기 위해 사용
+    
+    참고：http://glowingpython.blogspot.jp/2012/02/convolution-with-numpy.html
+    """
+    window_len = 11
+    s = np.r_[x[window_len-1:0:-1], x, x[-1:-window_len:-1]]
+    w = np.kaiser(window_len, 2)
+    y = np.convolve(w/w.sum(), s, mode='valid')
+    return y[5:len(y)-5]
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    
+    return y
+
+def cross_entropy_error(y, t):
+    # reference: https://github.com/llseungjun/deep-learning-from-scratch/blob/master/common/functions.py
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 값 복원
+        it.iternext()   
+        
+    return grad
